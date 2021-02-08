a6169ff4cddaa608e3062df1c96d0dc79d16431b
ecolotta <ecolotta@gmail.com>
2021-02-08 15:08:57 +0900

    Fix: trait名を修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     deadline { Random.rand(from..to) }
     association :project
 
-    trait :done_project do
+    trait :done do
       status { :done }
       completion_date { Time.current.yesterday }
     end
